82e14e347bd1b1a081cdf2b0eb1da36d397b0200
Herve Yu <yuhrvfr@gmail.com>
2023-04-25 12:48:44 -0700

    Added a simple connect netwoork check using google.com as test

--- py/vcs/check_all.py
+++ py/vcs/check_all.py
@@ -2,6 +2,8 @@
 import os
 import sys
 import shutil
+import socket
+
 
 def check_reboot():
     """Returns True if the computer has a pending reboot."""
@@ -21,11 +23,20 @@ def check_root_full():
     """Returns True if the root partition is full, False otherwise."""
     return check_disk_full(disk='/',min_gb=2, min_percent=10)
 
+def check_no_network():
+    """Returns True if it fails to resolve Google's URL, False otherwise"""
+    try:
+        socket.gethostbyname("www.google.com")
+        return False
+    except:
+        return True
+
 
 def main():
     checks=[
        (check_reboot,"Pending Reboot"),
-       (check_root_full,"Root partition full")
+       (check_root_full,"Root partition full"),
+       (check_no_network,"Cannot reach www.google.com"),
     ]
     everything_ok = True
     for check, msg in checks:
@@ -34,7 +45,7 @@ def main():
             everything_ok = False
     if not everything_ok:
         sys.exit(1)
-            
+
     print("Everything ok")
     sys.exit(0)
 
